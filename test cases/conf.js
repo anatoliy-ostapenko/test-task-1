78b755101437362f9d3766674141debdd67e6767
Anatolii Ostapenko <tec303@gmail.com>
2022-11-02 20:23:27 +0200

    first test

--- test cases/conf.js
+++ test cases/conf.js
@@ -12,7 +12,8 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['example_spec.js'],
+  //specs: ['example_spec.js'],
+  specs: ['test1.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
--- test cases/test1.js
+++ test cases/test1.js
@@ -1,29 +1,47 @@
+const { browser, element } = require("protractor");
+
 describe('', function() {
-    it('should greet the named user', function() {
-      browser.get('http://www.angularjs.org');
-  
-      element(by.model('yourName')).sendKeys('Julie');
-  
-      var greeting = element(by.binding('yourName'));
-  
-      expect(greeting.getText()).toEqual('Hello Julie!');
+    it('should greet the named user', async function() {
+      await browser.waitForAngularEnabled(false);
+      await browser.get('https://www.sbzend.ssls.com');
+      await expect(browser.getCurrentUrl()).toEqual("https://www.sbzend.ssls.com/");
+
+      await element(by.xpath('//*[@id="app"]/div/header/div/div/button[1]/span')).click();
+      await expect(browser.getCurrentUrl()).toEqual("https://www.sbzend.ssls.com/authorize");
+      await browser.sleep(2000);
+
+      await element(by.name('email')).click().sendKeys('ssls.automation+666@gmail.com');
+      await element(by.name('password')).click().sendKeys('123456');
+      await element(by.xpath('//*[@id="ng-app"]/body/div[1]/div/ui-view/div/ng-include/div/div/form/div[2]/div/div[1]/div[2]/button/span[1]')).click();
+      
+      //doesn't work
+      //await expect(element(by.name('password', 'qq')).isDisplayed()).toBe(true);
+
+
+    await expect(element(by.xpath('//*[@id="ng-app"]/body/div[1]/ssls-header/div/div/header/div/div/button[1]/span')).isDisplayed());
+    await element(by.xpath('//*[@id="ng-app"]/body/div[1]/div/ui-view/div/ng-include/div/div/form/div[3]/button')).click();
+    await browser.sleep(3000);
+
+    //await expect(element(by.xpath('//*[@id="app"]/div/header/div/div/div[1]/button/span')).isDispayed());
+    //await expect(element(by.xpath('//*[@id="app"]/div/header/div/div/div[1]/button/span')).isDisplayed());
+
+    //await element(by.text('ssls.automation+666@gmail.com')).click();
+
+    //await browser.sleep(2000);
+    
+    
+    
+    //await element(by.xpath('//*[@id="app"]/div/header/div/div/div[1]/div/ul/li[5]/button')).click();
+
     });
   
-    describe('todo list', function() {
-      var todoList;
-  
-      beforeEach(function() {
-        browser.get('http://www.angularjs.org');
-  
-        todoList = element.all(by.repeater('todo in todoList.todos'));
+     xit('should list todos', async function() {
+      await browser.waitForAngularEnabled(false);
+      await browser.get('https://www.sbzend.ssls.com');
+      await expect(browser.getCurrentUrl()).toEqual("https://www.sbzend.ssls.com/");
       });
   
-      it('should list todos', function() {
-        expect(todoList.count()).toEqual(2);
-        expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
-      });
-  
-      it('should add a todo', function() {
+      xit('should add a todo', function() {
         var addTodo = element(by.model('todoList.todoText'));
         var addButton = element(by.css('[value="add"]'));
   
@@ -33,6 +51,6 @@ describe('', function() {
         expect(todoList.count()).toEqual(3);
         expect(todoList.get(2).getText()).toEqual('write a protractor test');
       });
+
     });
-  });
   
